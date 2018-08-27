4a5a544bdaba29f044dc71298bb9375fb16a0e4b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 10:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-let newID = 1000000000 + oldID
+let newID = 1000000000 + oldID;
 
-ageIsValid = Math.round(currentAge)
+ageIsValid = Math.round(currentAge);
+
+randomNumber = random()*10)+1)
+
